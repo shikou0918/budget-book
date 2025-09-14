68999cdbe10c2244604146e59762455fb0f5d02c
shikou0918 <shikou.0918@gmail.com>
2025-09-14 13:14:06 +0900

    コンテナ起動時のヘルスチェック追加

--- backend/internal/infrastructure/database/connection.go
+++ backend/internal/infrastructure/database/connection.go
@@ -2,6 +2,8 @@ package database
 
 import (
 	"fmt"
+	"log"
+	"time"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 	"gorm.io/gorm/logger"
@@ -24,12 +26,25 @@ func NewConnection(config *Config) (*gorm.DB, error) {
 		config.Name,
 	)
 
-	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{
-		Logger: logger.Default.LogMode(logger.Info),
-	})
-	if err != nil {
-		return nil, fmt.Errorf("failed to connect to database: %w", err)
+	log.Printf("Attempting to connect to database with DSN: %s:****@tcp(%s:%s)/%s",
+		config.User, config.Host, config.Port, config.Name)
+
+	var db *gorm.DB
+	var err error
+
+	// Retry connection up to 30 times (30 seconds)
+	for i := 0; i < 30; i++ {
+		db, err = gorm.Open(mysql.Open(dsn), &gorm.Config{
+			Logger: logger.Default.LogMode(logger.Info),
+		})
+		if err == nil {
+			log.Printf("Successfully connected to database on attempt %d", i+1)
+			return db, nil
+		}
+
+		log.Printf("Failed to connect to database (attempt %d/30): %v", i+1, err)
+		time.Sleep(1 * time.Second)
 	}
 
-	return db, nil
+	return nil, fmt.Errorf("failed to connect to database after 30 attempts: %w", err)
 }
