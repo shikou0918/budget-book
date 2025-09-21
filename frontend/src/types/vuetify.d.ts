22bb611a7591f6292891a7b43f78dc2cf9a8f862
shikou0918 <shikou.0918@gmail.com>
2025-09-22 08:08:27 +0900

    CIエラー対応

---
+++ frontend/src/types/vuetify.d.ts
@@ -0,0 +1,16 @@
+declare module 'vuetify/styles' {
+  const content: any;
+  export default content;
+}
+
+declare module 'vuetify/components' {
+  const components: any;
+  export default components;
+  export * from 'vuetify/components';
+}
+
+declare module 'vuetify/directives' {
+  const directives: any;
+  export default directives;
+  export * from 'vuetify/directives';
+}
