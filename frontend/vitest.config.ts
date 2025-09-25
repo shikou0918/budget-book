285b3a1ec57f1e344fdcf9a37d6c1dad31202b86
shikou0918 <shikou.0918@gmail.com>
2025-09-26 08:47:58 +0900

    vitest.config.ts 追加

---
+++ frontend/vitest.config.ts
@@ -0,0 +1,28 @@
+import { defineConfig } from 'vitest/config'
+import vue from '@vitejs/plugin-vue'
+import vuetify from 'vite-plugin-vuetify'
+import { fileURLToPath, URL } from 'node:url'
+
+export default defineConfig({
+  plugins: [
+    vue(),
+    vuetify()
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  test: {
+    environment: 'jsdom',
+    globals: true,
+    setupFiles: ['./src/test-setup.ts'],
+    css: false,
+    deps: {
+      inline: ['vuetify']
+    }
+  },
+  define: {
+    'process.env': {}
+  }
+})
